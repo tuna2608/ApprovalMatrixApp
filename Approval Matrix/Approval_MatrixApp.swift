0019657a5d49cc86207014c4b96d77b8efac59f1
tuna2608 <tuna1.dev@gmail.com>
2023-05-09 09:46:54 +0700

    Initial Commit

---
+++ Approval Matrix/Approval_MatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  Approval_MatrixApp.swift
+//  Approval Matrix
+//
+//  Created by Anh Tú on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Approval_MatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
